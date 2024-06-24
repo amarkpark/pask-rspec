a68b64b49e89b651f8dcd4fd43ce8e896774c183
Amanda Mark <amarkpark@gmail.com>
2024-06-24 15:15:29 -0600

    Lesson 16 - Moving on to Intermediate fail state again
    I'm adding a before hook because I can

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,9 +6,29 @@ class Card
   end
 end
 
-RSpec.describe Card do
-  it "has a type" do
-    card = Card.new("AS")
-    expect(card.type).to eq("AS")
+RSpec.configure do |config|
+  config.before(:all) do
+    card = Card.new("A", "S")
   end
 end
+
+RSpec.describe Card do
+  # it "has a type" do
+  #   card = Card.new("AS")
+  #   expect(card.type).to eq("AS")
+  # end
+
+  it "has a rank" do
+    expect(card.rank).to eq("A")
+  end
+
+  it "has a suit" do
+    expect(card.suit).to eq("S")
+  end
+end
+
+# RSpec.describe do
+#   it "can calculate" do
+#     expect(1+2).to eq(4)
+#   end
+# end
